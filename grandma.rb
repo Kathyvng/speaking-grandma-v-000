652e4a21383577ecac4958af123118ea487f6c23
Kathy Nguyen <kathyvng@gmail.com>
2018-06-19 04:35:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == downcase
+  if phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == upcase
+  else phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
