e6b1c15fde821c934239b8038ff39df8b5ba21e5
Kathy Nguyen <kathyvng@gmail.com>
2018-06-19 04:49:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,8 @@ def speak_to_grandma(phrase)
   else
       return "HUH?! SPEAK UP, SONNY!"
   end
-  
-  if phrase = ["I LOVE YOU GRANDMA!"]
+
+  if phrase == ["I LOVE YOU GRANDMA!"]
       return "I LOVE YOU TOO PUMPKIN!"
   end
 end
