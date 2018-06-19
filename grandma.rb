817f1520132ea4d17ec8b05034bc530594284c7e
Kathy Nguyen <kathyvng@gmail.com>
2018-06-19 04:47:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
   else
-      puts "HUH?! SPEAK UP, SONNY!"
+      return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
